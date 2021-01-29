23be264bc18fb7752a16266215ea59c23e11e18b
Igor Esposito <68132154+Sposigor@users.noreply.github.com>
2021-01-29 11:22:51 -0300

    Add files via upload

---
+++ dados.py
@@ -0,0 +1,68 @@
+# Bibliotecas necessarias para execução da função
+
+import streamlit as st
+import pandas as pd
+import numpy
+import quandl as q
+
+# streamlit cache basicamente é para manter a infomação no cache do navegador para quando houve a seleção dos indicadores não tenha que recarregar.
+
+@st.cache()
+def get_dados(): # Função para usar a API e extrair os dados e uma pequena transformação nas colunas.
+
+    # Safras
+    # Usei a função het do quandl para puxar a infomação da base de dados deles e com isso ter acesso as atualizações diarias pela API.
+    Gado = pd.DataFrame(q.get('CEPEA/CATTLE', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Gado.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True)
+
+    Bezerro = pd.DataFrame(q.get('CEPEA/CALF', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Bezerro.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True)
+
+    Algodão = pd.DataFrame(q.get('CEPEA/COTTON', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Algodão.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True)
+
+    Laranja_Industrial = pd.DataFrame(q.get('CEPEA/EOR_DP', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Laranja_Industrial.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True)
+
+    Laranja_in_Natura = pd.DataFrame(q.get('CEPEA/POR_NM', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Laranja_in_Natura.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True)
+
+    Porco = pd.DataFrame(q.get('CEPEA/PORK', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Porco.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True)
+
+    Frango_Congelado = pd.DataFrame(q.get('CEPEA/POULTRY_C', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Frango_Congelado.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True)
+
+    Frango_Frio = pd.DataFrame(q.get('CEPEA/POULTRY_F', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Frango_Frio.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True)
+
+    Arroz = pd.DataFrame(q.get('CEPEA/RICE', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Arroz.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True)
+
+    Soja = pd.DataFrame(q.get('CEPEA/SOYBEAN', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Soja.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True)
+
+    Soja_C = pd.DataFrame(q.get('CEPEA/SOYBEAN_C', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Soja_C.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True)
+
+    Açucar = pd.DataFrame(q.get('CEPEA/SUGAR', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Açucar.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True) 
+
+    Trigo_P = pd.DataFrame(q.get('CEPEA/WHEAT_P', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Trigo_P.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True)
+
+    Trigo_R = pd.DataFrame(q.get('CEPEA/WHEAT_R', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Price US$'])
+    Trigo_R.rename(columns={'Date':'Data', 'Price US$':'Preço US$'}, inplace=True)
+
+    # Commoditys
+
+    Café_R = pd.DataFrame(q.get('CEPEA/COFFEE_R', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Cash Price US$'])
+    Café_R.rename(columns={'Date':'Data', 'Cash Price US$':'Preço US$'}, inplace=True)
+
+    Café_A = pd.DataFrame(q.get('CEPEA/COFFEE_A', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Cash Price US$'])
+    Café_A.rename(columns={'Date':'Data', 'Cash Price US$':'Preço US$'}, inplace=True)
+    
+    Milho = pd.DataFrame(q.get('CEPEA/CORN', authtoken="-zKjekRSoo3qjzzCVyUS", returns='numpy'), columns=['Date', 'Cash Price US$'])
+    Milho.rename(columns={'Date':'Data', 'Cash Price US$':'Preço US$'}, inplace=True) 
+
+    return Gado, Bezerro, Algodão, Laranja_Industrial, Laranja_in_Natura, Porco, Frango_Congelado, Frango_Frio, Arroz, Soja, Soja_C, Açucar, Trigo_P, Trigo_R, Café_A, Café_R, Milho
--- graf.py
+++ graf.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime
 import plotly.graph_objects as go
 import pandas as pd
+import streamlit as st
 
 def plot(titulo:str, df: pd.DataFrame):
   
@@ -52,132 +53,12 @@ def plot(titulo:str, df: pd.DataFrame):
           showline=False,
           showticklabels=False,
       ),
-      autosize=False,
-      margin=dict(
-          autoexpand=False,
-          l=100,
-          r=20,
-          t=110,
-      ),
-      showlegend=False,
-      plot_bgcolor='white'
-  )
-
-  # Variaveis
-  annotations = []
-  crecimento = (y_data.iloc[-1] - y_data[0])/y_data[0]
-
-  # Ajusta o lado esquerdo do grafico
-  annotations.append(dict(xref='paper', x=0.05, y=y_data[0],
-                                    xanchor='right', yanchor='middle',
-                                    text=labels + ' {}'.format(y_data[0]),
-                                    font=dict(family='Arial',
-                                              size=14),
-                                    showarrow=False))
-  # Ajusta o lado direito do grafico
-  annotations.append(dict(xref='paper', x=0.95, y=y_data.iloc[-1],
-                                    xanchor='left', yanchor='middle',
-                                    text=labels +' {}'.format(y_data.iloc[-1]),
-                                    font=dict(family='Arial',
-                                              size=14),
-                                    showarrow=False))
-  # Titulo
-  annotations.append(dict(xref='paper', yref='paper', x=0.0, y=1.05,
-                                xanchor='left', yanchor='bottom',
-                                text=titulo,
-                                font=dict(family='Arial',
-                                          size=30,
-                                          color='rgb(37,37,37)'),
-                                showarrow=False))
-  # Fonte
-  annotations.append(dict(xref='paper', yref='paper', x=0.5, y=-0.1,
-                                xanchor='center', yanchor='top',
-                                text='Fonte: CEPEA',
-                                font=dict(family='Arial',
-                                          size=12,
-                                          color='rgb(150,150,150)'),
-                                showarrow=False))
-  # Data
-  annotations.append(dict(xref='paper', yref='paper', x=1, y=-0.1,
-                                xanchor='right', yanchor='top',
-                                text='{}'.format(Hoje.strftime("%B %d, %Y")),
-                                font=dict(family='Arial',
-                                          size=12,
-                                          color='rgb(150,150,150)'),
-                                showarrow=False))
-  # Crescimento %
-  annotations.append(dict(xref='paper', yref='paper', x=1, y=0.1,
-                                xanchor='right', yanchor='top',
-                                text='Crecimento de {:.2f}% em relação ao valor inicial'.format(crecimento*100),
-                                font=dict(family='Arial',
-                                          size=12,
-                                          color='rgb(150,150,150)'),
-                                showarrow=False))
-
-
-
-
-  fig.update_layout(annotations=annotations)
-
-  return fig.show()
-
-
-def plotC(titulo:str, df: pd.DataFrame):
-  
-  # Variaveis 
-  titulo = titulo
-  labels = 'US$'
-  Hoje = datetime.now()
-  data = df
-  mode_size = 12
-  line_size = 4
-  x_data = data.Data
-  y_data = data['Cash Price US$']
-
-  # Grafico
-  fig = go.Figure()
-
-  fig.add_trace(go.Scatter(x=x_data, y=y_data, mode='lines',
-          name=labels,
-          line=dict(color='rgb(49,130,189)', width=line_size),
-          connectgaps=True,
-      ))
-
-      # Pontos Inicial e Final para marcação  
-  fig.add_trace(go.Scatter(
-      x=[x_data[0], x_data.iloc[-1]],
-      y=[y_data[0], y_data.iloc[-1]],
-      mode='markers',
-      marker=dict(color='rgb(49,130,189)', size=mode_size)
-      ))
-  
-
-  fig.update_layout(
-      xaxis=dict(
-          showline=True,
-          showgrid=False,
-          showticklabels=True,
-          linecolor='rgb(204, 204, 204)',
-          linewidth=2,
-          ticks='outside',
-          tickfont=dict(
-              family='Arial',
-              size=12,
-              color='rgb(82, 82, 82)',
-          ),
-      ),
-      yaxis=dict(
-          showgrid=False,
-          zeroline=False,
-          showline=False,
-          showticklabels=False,
-      ),
       autosize=True,
       margin=dict(
           autoexpand=True,
-          l=100,
-          r=20,
-          t=110,
+          l=50,
+          r=50,
+          t=60,
       ),
       showlegend=False,
       plot_bgcolor='white'
@@ -226,7 +107,7 @@ def plotC(titulo:str, df: pd.DataFrame):
                                           color='rgb(150,150,150)'),
                                 showarrow=False))
   # Crescimento %
-  annotations.append(dict(xref='paper', yref='paper', x=1, y=0.1,
+  annotations.append(dict(xref='paper', yref='paper', x=1, y=0.05,
                                 xanchor='right', yanchor='top',
                                 text='Crecimento de {:.2f}% em relação ao valor inicial'.format(crecimento*100),
                                 font=dict(family='Arial',
@@ -239,4 +120,4 @@ def plotC(titulo:str, df: pd.DataFrame):
 
   fig.update_layout(annotations=annotations)
 
-  return fig.show()
+  return  st.plotly_chart(fig, use_container_width=True)
